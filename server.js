0434e9bc8288c045e78dc5374b8c36f0965000ff
Chris Dooley <chris.dooley@amt-sybex.com>
2017-10-12 13:06:33 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+//Create app
+var app=express();
+
+//set express configurations
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+  //Log time
+  var now=new Date().toString();
+  var log=`${now}: ${req.method} ${req.url}`
+
+  console.log(log)
+  fs.appendFile('server.log',log + '\n',(err)=>{
+    if(err){
+      console.log('unable to append to server log')
+    }
+  });
+
+  next();
+});
+
+//maintenance middleware, no NEXT so nothing beyond this gets executed
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs');
+// });
+
+//configure middleware
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+//Set up HTTP route handlers
+app.get('/Home',(req,res)=>{
+    // response.send('<h1>HelloExpress!</h1>');
+    res.render('home.hbs',{
+      pageTitle:'home page',
+      welcomeMessage:'Howdy'
+    });
+});
+
+app.get('/about',(req,res)=>{
+  res.render('About.hbs',{
+    pageTitle:'about page',
+  });
+});
+
+app.get('/bad',(req,res)=>{
+  res.send(
+    {
+      errorMessage:'Unable to handle request'
+    }
+  );
+});
+//Add listener, bind to port on machine
+app.listen(3000,()=>{
+  console.log('server is up on port 3000');
+});
