389b2726f1255d0cd5ba3fb8fb86e03753f81d42
Chris Dooley <chris.dooley@amt-sybex.com>
2017-10-18 08:58:25 +0100

    fix Heroku issue

--- server.js
+++ server.js
@@ -1,68 +1,56 @@
-const express=require('express');
-const hbs=require('hbs');
-const fs=require('fs');
-const port=process.env.PORT ||3000;
-//Create app
-var app=express();
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
-//set express configurations
-hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine','hbs');
+const port = process.env.PORT || 3000;
+var app = express();
 
-app.use((req,res,next)=>{
-  //Log time
-  var now=new Date().toString();
-  var log=`${now}: ${req.method} ${req.url}`
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 
-  console.log(log)
-  fs.appendFile('server.log',log + '\n',(err)=>{
-    if(err){
-      console.log('unable to append to server log')
-    }
-  });
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
 
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
   next();
 });
 
-//maintenance middleware, no NEXT so nothing beyond this gets executed
-// app.use((req,res,next)=>{
+// app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
 
-//configure middleware
 app.use(express.static(__dirname + '/public'));
 
-hbs.registerHelper('getCurrentYear',()=>{
-  return new Date().getFullYear()
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
 });
 
-hbs.registerHelper('screamIt',(text)=>{
+hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
-//Set up HTTP route handlers
-app.get('/Home',(req,res)=>{
-    // response.send('<h1>HelloExpress!</h1>');
-    res.render('home.hbs',{
-      pageTitle:'home page',
-      welcomeMessage:'Howdy'
-    });
-});
-
-app.get('/about',(req,res)=>{
-  res.render('About.hbs',{
-    pageTitle:'about page',
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
   });
 });
 
-app.get('/bad',(req,res)=>{
-  res.send(
-    {
-      errorMessage:'Unable to handle request'
-    }
-  );
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
-//Add listener, bind to port on machine
-app.listen(port,()=>{
-  console.log(`server is up on port ${port}`);
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
